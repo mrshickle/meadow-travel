c2c107462edc811532c2f1e11f34307c0237e412
mrshickle <tawbsl@gmail.com>
2014-12-30 17:14:26 -0500

    Some QA, Layouts, Partials

--- mw.js
+++ mw.js
@@ -2,13 +2,65 @@ var express = require('express');
 var fortune = require('./lib/fortune.js');
 var app = express();
 
-var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
+var handlebars = require('express3-handlebars').create({ 
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){ 
+            if(!this._sections) this._sections = {}; 
+            this._sections[name] = options.fn(this); 
+            return null;
+        } 
+    }
+});
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public')); 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+    next();
+});
+
+
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland', 
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
+    next();
+});
+
 
 app.get('/', function(req, res){ 
     res.render('home');
@@ -16,8 +68,20 @@ app.get('/', function(req, res){
 
 app.get('/about', function(req, res){
 
-    res.render('about', { fortune: fortune.getFortune() } );
+    res.render('about', { 
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    } );
 });
+
+app.disable('x-powered-by');
+
+app.get('/headers', function(req,res){
+    res.set('Content-Type','text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n'; res.send(s);
+});
+
 
 // custom 404 page
 app.use(function(req, res){ 
@@ -32,15 +96,12 @@ app.use(function(err, req, res, next){
     res.render('500');   
 });
 
+
+
+
 app.listen(app.get('port'), function(){
   console.log( 'Express started on http://localhost:' +
-  app.get('port') + '; press Ctrl-C to terminate.' );
+      app.get('port') + '; press Ctrl-C to terminate.' );
 });
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.", 
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.", 
-    "Whenever possible, keep it simple.",
-];
+
